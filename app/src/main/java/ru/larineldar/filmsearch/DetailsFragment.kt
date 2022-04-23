3ab672f2d010fee9e0be60a4f3d2fa0cb84e681c
LarinEldar <larineldar@gmail.com>
2022-04-23 07:45:18 +0400

    homework 26

--- app/src/main/java/ru/larineldar/filmsearch/DetailsFragment.kt
+++ app/src/main/java/ru/larineldar/filmsearch/DetailsFragment.kt
@@ -1,5 +1,6 @@
 package ru.larineldar.filmsearch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DetailsFragment : Fragment() {
 
@@ -25,9 +27,33 @@ class DetailsFragment : Fragment() {
         val image = view.findViewById<AppCompatImageView>(R.id.details_poster)
         val description = view.findViewById<TextView>(R.id.details_description)
         val title = view.findViewById<TextView>(R.id.details_title)
-
+        val favorite = view.findViewById<FloatingActionButton>(R.id.details_fab_favorites)
+        val fabShare = view.findViewById<FloatingActionButton>(R.id.details_fab)
         image.setImageResource(film.poster)
         description.text = film.description
         title.text = film.title
+
+        favorite.setImageResource(
+            if(film.isInFavorites)
+                R.drawable.ic_baseline_favorite_24
+            else
+                R.drawable.ic_baseline_favorite_border_24
+        )
+        favorite.setOnClickListener{
+            if (!film.isInFavorites) {
+                favorite.setImageResource(R.drawable.ic_baseline_favorite_24)
+                film.isInFavorites = true
+            } else {
+                favorite.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                film.isInFavorites = false
+            }
+        }
+        fabShare.setOnClickListener{
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT,
+                "Check out this film: ${film.title} \n\n ${film.description}")
+            intent.type = "text/plain"
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ru/larineldar/filmsearch/FavoritesFragment.kt
@@ -0,0 +1,29 @@
+package ru.larineldar.filmsearch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class FavoritesFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.favorites_recycler)
+        recyclerView.adapter = FilmAdapter()
+        recyclerView.addItemDecoration(SpacingItemDecoration(8))
+    }
+}
